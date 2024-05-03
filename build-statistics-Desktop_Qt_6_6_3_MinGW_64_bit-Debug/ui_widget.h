fd12e78b1d237f9330807ab06639832874a4e5d8
Simanenka_Alex <simonenkoalexmarik@gmail.com>
2024-05-03 23:35:48 +0300

    ftr

---
+++ build-statistics-Desktop_Qt_6_6_3_MinGW_64_bit-Debug/ui_widget.h
@@ -0,0 +1,59 @@
+/********************************************************************************
+** Form generated from reading UI file 'widget.ui'
+**
+** Created by: Qt User Interface Compiler version 6.6.3
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_WIDGET_H
+#define UI_WIDGET_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Widget
+{
+public:
+    QPushButton *pushButton;
+    QCheckBox *checkBox;
+
+    void setupUi(QWidget *Widget)
+    {
+        if (Widget->objectName().isEmpty())
+            Widget->setObjectName("Widget");
+        Widget->resize(800, 600);
+        pushButton = new QPushButton(Widget);
+        pushButton->setObjectName("pushButton");
+        pushButton->setGeometry(QRect(210, 290, 83, 29));
+        checkBox = new QCheckBox(Widget);
+        checkBox->setObjectName("checkBox");
+        checkBox->setGeometry(QRect(450, 270, 93, 26));
+
+        retranslateUi(Widget);
+        QObject::connect(pushButton, &QPushButton::clicked, checkBox, qOverload<>(&QCheckBox::click));
+
+        QMetaObject::connectSlotsByName(Widget);
+    } // setupUi
+
+    void retranslateUi(QWidget *Widget)
+    {
+        Widget->setWindowTitle(QCoreApplication::translate("Widget", "Widget", nullptr));
+        pushButton->setText(QCoreApplication::translate("Widget", "PushButton", nullptr));
+        checkBox->setText(QCoreApplication::translate("Widget", "CheckBox", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Widget: public Ui_Widget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_WIDGET_H
